f22c0953e9b67e66b2b75cda4d4108815f358918
Mihaela <Mihaela@DESKTOP-40004PU>
2024-08-19 19:08:57 +0300

    Add project files.

---
+++ HereHaveAPaSSword/GeneratePasswordsService.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HereHaveAPaSSword
+{
+    public class GeneratePasswordsService
+    {
+        public GeneratePasswordsService() { }
+
+        public List<string> GetPasswords(int totalCharacters, int alphabetMin, int alphabetMax, int numericMin, int numericMax, int specialMin, int specialMax)
+        {
+            var theLfsrString = GenerateSeed();
+            List<string> passwordList = new List<string>();
+
+            for (int i = 0; i < 50; i++)
+            {
+                var receivedTuple = GetOnePassword(totalCharacters, alphabetMin, alphabetMax, numericMin, numericMax, specialMin, specialMax, theLfsrString);
+                var password = receivedTuple.Item1;
+                theLfsrString = receivedTuple.Item2;
+                passwordList.Add(password);
+            }
+
+            return passwordList;
+        }
+
+        private Tuple<string, string> GetOnePassword(int totalCharacters, int alphabetMin, int alphabetMax, int numericMin, int numericMax, int specialMin, int specialMax, string theLfsrString)
+        {
+            string newPassword = "";
+            int alphabet = 0;
+            int numeric = 0;
+            int special = 0;
+
+            for (int i = 0; i < alphabetMin; i++)
+            {
+                var caseTuple = GetRandomNumber(theLfsrString);
+                int upperCase = caseTuple.Item1 % 2;
+                theLfsrString = caseTuple.Item2;
+
+                var recievedTuple = GetRandomNumber(theLfsrString);
+                int randomNumber = recievedTuple.Item1 % 26 + ('a') * (1 - upperCase) + ('A') *upperCase;
+                theLfsrString = recievedTuple.Item2;
+
+                newPassword += Convert.ToChar(randomNumber);
+                alphabet++;
+            }
+
+            for (int i = 0; i < numericMin; i++)
+            {
+                var recievedTuple = GetRandomNumber(theLfsrString);
+                int randomNumber = recievedTuple.Item1 % 10;
+                theLfsrString = recievedTuple.Item2;
+
+                newPassword += randomNumber;
+                numeric++;
+            }
+
+            string specialCharacters = "!#$%&()*+,-./:;?@[]^_`{|}";
+            for (int i = 0; i < specialMin; i++)
+            {
+                var recievedTuple = GetRandomNumber(theLfsrString);
+                int randomNumber = recievedTuple.Item1 % specialCharacters.Length;
+                theLfsrString = recievedTuple.Item2;
+                newPassword += specialCharacters[randomNumber];
+                special++;
+            }
+
+            while (totalCharacters > newPassword.Length)
+            {
+                var someTuple = GetRandomNumber(theLfsrString);
+                int characterType = someTuple.Item1 % 20;
+                theLfsrString = someTuple.Item2;
+
+                if (characterType < 10 && (alphabetMax == -1 || alphabet < alphabetMax))
+                {
+                    var caseTuple = GetRandomNumber(theLfsrString);
+                    int upperCase = caseTuple.Item1 % 2;
+                    theLfsrString = caseTuple.Item2;
+
+                    var recievedTuple = GetRandomNumber(theLfsrString);
+                    int randomNumber = recievedTuple.Item1 % 26 + ('a') * (1 - upperCase) + ('A') * upperCase;
+                    theLfsrString = recievedTuple.Item2;
+
+                    newPassword += Convert.ToChar(randomNumber);
+                    alphabet++;
+                }
+
+                if (totalCharacters == newPassword.Length) break;
+
+                if (characterType > 10 && (numericMax == -1 || numeric < numericMax))
+                {
+                    var recievedTuple = GetRandomNumber(theLfsrString);
+                    int randomNumber = recievedTuple.Item1 % 10;
+                    theLfsrString = recievedTuple.Item2;
+
+                    newPassword += randomNumber;
+                    numeric++;
+                }
+
+                if (totalCharacters == newPassword.Length) break;
+
+                if (characterType == 10 && (specialMax == -1 || special < specialMax))
+                {
+                    var recievedTuple = GetRandomNumber(theLfsrString);
+                    int randomNumber = recievedTuple.Item1 % specialCharacters.Length;
+                    theLfsrString = recievedTuple.Item2;
+                    newPassword += specialCharacters[randomNumber];
+                    special++;
+                }
+            }
+
+            var receivedTuple = ShufflePassword(newPassword, theLfsrString);
+
+            return Tuple.Create(receivedTuple.Item1, receivedTuple.Item2);
+        }
+
+        private Tuple<string, string> ShufflePassword(string password, string theLfsrString)
+        {
+            var shuffledPassword = "";
+            var initialLenght = password.Length;
+
+            for (int i = 0; i < initialLenght; i++)
+            {
+                var receivedTuple = GetRandomNumber(theLfsrString);
+                var number = receivedTuple.Item1 % password.Length;
+                theLfsrString = receivedTuple.Item2;
+                shuffledPassword += password[number];
+                password = password.Remove(number, 1);
+            }
+
+            return Tuple.Create(shuffledPassword, theLfsrString);
+        }
+
+        private String GenerateSeed()
+        {
+            var currentTime = DateTime.Now;
+            string scrambledDate = currentTime.Month.ToString() + currentTime.Day.ToString() + currentTime.Hour.ToString() + currentTime.Year.ToString() + currentTime.Millisecond.ToString() + currentTime.Minute.ToString();
+            string fileName = "SpecialImage.png";
+            scrambledDate += System.IO.Path.GetFullPath(fileName).ToString();
+            scrambledDate += currentTime.Ticks.ToString();
+
+            SHA256 mySHA256 = SHA256.Create();
+            var hashedSeed = mySHA256.ComputeHash(Encoding.UTF8.GetBytes(scrambledDate));
+
+            StringBuilder seed = new StringBuilder();
+            foreach (byte b in hashedSeed)
+            {
+                seed.Append(b.ToString("x2"));
+            }
+
+            string resultedString = "";
+            foreach (char ch in seed.ToString())
+            {
+                resultedString += CharToBinary((char)ch);
+            }
+
+            return resultedString;
+        }
+
+        private string CharToBinary(char c)
+        {
+            var number = Convert.ToInt32(c);
+            return Convert.ToString(number, 2);
+        }
+
+        private Tuple<int, string> GetRandomNumber(string theLfsrString)
+        {
+            var receivedTuple = LFSR(theLfsrString);
+            var generateRandomBytes = receivedTuple.Item1;
+            theLfsrString = receivedTuple.Item2;
+
+            int number = 0;
+            for (int i = 0; i < 22; i++)
+            {
+                number *= 2;
+                if (generateRandomBytes[i] == '1') { number++; }
+            }
+
+            return Tuple.Create(number, theLfsrString);
+        }
+
+        private char XorOperation(char a, char b)
+        {
+            if (a == b)
+            {
+                return '0';
+            }
+            return '1';
+        }
+
+        private char MultipleXOR(string myArray)
+        {
+            char rez = myArray[2];
+            for (int i = 4; i <= 15; i += 2)
+            {
+                rez = XorOperation(rez, myArray[i]);
+            }
+            return rez;
+        }
+
+        private Tuple<string, string> LFSR(string myArray)
+        {
+            string generatedArray = "";
+            while (true)
+            {
+                generatedArray += myArray[myArray.Length - 1];
+
+                if (generatedArray.Length == 22)
+                {
+                    break;
+                }
+
+                char rezXor = MultipleXOR(myArray);
+
+                myArray = myArray.Remove(myArray.Length - 1, 1);
+                myArray = rezXor + myArray;
+            }
+            return Tuple.Create(generatedArray, myArray);
+        }
+    }
+}
---
+++ HereHaveAPaSSword/MainWindow.xaml.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlTypes;
+
+namespace HereHaveAPaSSword
+{
+
+    public partial class MainWindow : Window
+    {
+        private GeneratePasswordsService _generatePasswordsService;
+        public MainWindow()
+        {
+            _generatePasswordsService = new GeneratePasswordsService();
+            InitializeComponent();
+        }
+
+        bool IsDigitsOnly(string str)
+        {
+            foreach (char c in str)
+            {
+                if (c == ' ')
+                    continue;
+
+                if (c < '0' || c > '9')
+                    return false;
+            }
+            return true;
+        }
+
+        private void GeneratePasswords(object sender, RoutedEventArgs e)
+        {
+            RefreshTextBoxes();
+            PasswordList.ItemsSource = null;
+
+            if (string.IsNullOrWhiteSpace(TotalCharacters.Text)) {
+                ErrorsLabel.Background = Brushes.White;
+                TotalCharacters.Background = Brushes.LightPink;
+                ErrorsTextBlock.Text = "Numarul total de caractere este obligatoriu!!!!";
+                return;
+            }
+
+            if (!IsDigitsOnly(AlphabetMin.Text) || !IsDigitsOnly(AlphabetMax.Text) ||
+                !IsDigitsOnly(NumericMin.Text) || !IsDigitsOnly(NumericMax.Text) || 
+                !IsDigitsOnly(SpecialMin.Text) || !IsDigitsOnly(SpecialMax.Text) || !IsDigitsOnly(TotalCharacters.Text))
+            {
+                if (!IsDigitsOnly(AlphabetMin.Text)) { AlphabetMin.Background = Brushes.LightPink; };
+                if (!IsDigitsOnly(AlphabetMax.Text)) { AlphabetMax.Background = Brushes.LightPink; };
+                if (!IsDigitsOnly(NumericMin.Text)) { NumericMin.Background = Brushes.LightPink; };
+                if (!IsDigitsOnly(NumericMax.Text)) { NumericMax.Background = Brushes.LightPink; };
+                if (!IsDigitsOnly(SpecialMin.Text)) { SpecialMin.Background = Brushes.LightPink; };
+                if (!IsDigitsOnly(SpecialMax.Text)) { SpecialMax.Background = Brushes.LightPink; };
+                if (!IsDigitsOnly(TotalCharacters.Text)) { TotalCharacters.Background = Brushes.LightPink; };
+
+                ErrorsLabel.Background = Brushes.White;
+                ErrorsTextBlock.Text = "Se pot introduce doar cifre sau spatii goale!!!!";
+                return;
+            }
+
+            var alphabetMin = string.IsNullOrWhiteSpace(AlphabetMin.Text) ? -1 : Int32.Parse(AlphabetMin.Text.Replace(" ", ""));
+            var alphabetMax = string.IsNullOrWhiteSpace(AlphabetMax.Text) ? -1 : Int32.Parse(AlphabetMax.Text.Replace(" ", ""));
+            var numericMin = string.IsNullOrWhiteSpace(NumericMin.Text) ? -1 : Int32.Parse(NumericMin.Text.Replace(" ", ""));
+            var numericMax = string.IsNullOrWhiteSpace(NumericMax.Text) ? -1 : Int32.Parse(NumericMax.Text.Replace(" ", ""));
+            var specialMin = string.IsNullOrWhiteSpace(SpecialMin.Text) ? -1 : Int32.Parse(SpecialMin.Text.Replace(" ", ""));
+            var specialMax = string.IsNullOrWhiteSpace(SpecialMax.Text) ? -1 : Int32.Parse(SpecialMax.Text.Replace(" ", ""));
+            var totalCharacters = Int32.Parse(TotalCharacters.Text.Replace(" ", ""));
+
+            if (!ValidateNumbers(totalCharacters, alphabetMin, alphabetMax, numericMin, numericMax, specialMin, specialMax))
+            {
+                return;
+            }
+            
+            PasswordList.ItemsSource = _generatePasswordsService.GetPasswords(totalCharacters, alphabetMin,alphabetMax, numericMin, numericMax, specialMin, specialMax);
+
+        }
+
+        private bool ValidateNumbers(int totalCharacters, int alphabetMin, int alphabetMax, int numericMin, int numericMax, int specialMin, int specialMax)
+        {
+            if (totalCharacters > 30 || totalCharacters < 6)
+            {
+                TotalCharacters.Background = Brushes.LightPink;
+                ErrorsLabel.Background = Brushes.White;
+                ErrorsTextBlock.Text = "Sunt permise parole de minim 6 si maxim 30 de caractere!!";
+                return false;
+            }
+
+            if (specialMax != -1 && alphabetMax != -1 && numericMax != -1 && specialMax + alphabetMax + numericMax < totalCharacters)
+            {
+                AlphabetMax.Background = Brushes.LightPink;
+                NumericMax.Background = Brushes.LightPink;
+                SpecialMax.Background = Brushes.LightPink;
+                ErrorsLabel.Background = Brushes.White;
+                ErrorsTextBlock.Text = "Numarul de caractere maxim este mai mic decat numarul total de caractere dorite!!!!";
+                return false;
+            }
+
+            bool caractereMinime = true;
+            if (specialMin != -1 && alphabetMin != -1 && numericMin != -1 && specialMin + alphabetMin + numericMin > totalCharacters)
+            {
+                AlphabetMin.Background = Brushes.LightPink;
+                NumericMin.Background = Brushes.LightPink;
+                SpecialMin.Background = Brushes.LightPink;
+                caractereMinime =  false;
+            }
+
+            if (alphabetMin != -1 && alphabetMin > totalCharacters){AlphabetMin.Background = Brushes.LightPink; caractereMinime = false; }
+            if (numericMin != -1 && numericMin > totalCharacters) { NumericMin.Background = Brushes.LightPink; caractereMinime = false; }
+            if (specialMin != -1 && specialMin > totalCharacters) { SpecialMin.Background = Brushes.LightPink; caractereMinime = false; }
+
+            if (alphabetMin != -1 && numericMin != -1 && alphabetMin + numericMin > totalCharacters) { 
+                AlphabetMin.Background = Brushes.LightPink; NumericMin.Background = Brushes.LightPink; caractereMinime = false;
+            }
+
+            if (specialMin != -1 && numericMin != -1 && specialMin + numericMin > totalCharacters)
+            {
+                SpecialMin.Background = Brushes.LightPink; NumericMin.Background = Brushes.LightPink; caractereMinime = false;
+            }
+
+            if (alphabetMin != -1 && specialMin != -1 && alphabetMin + specialMin > totalCharacters)
+            {
+                AlphabetMin.Background = Brushes.LightPink; SpecialMin.Background = Brushes.LightPink; caractereMinime = false;
+            }
+
+            if (!caractereMinime)
+            {
+                ErrorsLabel.Background = Brushes.White;
+                ErrorsTextBlock.Text = "Numarul de caractere minim este mai mare decat numarul total de caractere dorite!!!!";
+                return false;
+            }
+
+            bool comparareNumere = true;
+            if (alphabetMax != -1 && alphabetMin != -1 && alphabetMin > alphabetMax)
+            {
+                AlphabetMin.Background = Brushes.LightPink;
+                AlphabetMax.Background = Brushes.LightPink;
+                comparareNumere = false;
+            }
+
+            if (numericMax != -1 && numericMin != -1 && numericMin > numericMax)
+            {
+                NumericMin.Background = Brushes.LightPink;
+                NumericMax.Background = Brushes.LightPink;
+                comparareNumere = false;
+            }
+
+            if (specialMax != -1 && specialMin != -1 && specialMin > specialMax)
+            {
+                SpecialMin.Background = Brushes.LightPink;
+                SpecialMax.Background = Brushes.LightPink;
+                comparareNumere = false;
+            }
+
+            if (!comparareNumere)
+            {
+                ErrorsLabel.Background = Brushes.White;
+                ErrorsTextBlock.Text = "Numarul minim de caractere de un fel nu poate fi mai mare decat maximul de caractere de acelasi fel";
+                return false;
+            }
+
+            return true; ;
+        }
+
+        private void RefreshTextBoxes ()
+        {
+            AlphabetMin.Background = Brushes.White;
+            AlphabetMax.Background = Brushes.White;
+            NumericMin.Background = Brushes.White;
+            NumericMax.Background = Brushes.White;
+            SpecialMin.Background = Brushes.White;
+            SpecialMax.Background = Brushes.White;
+            TotalCharacters.Background = Brushes.White;
+            ErrorsLabel.Background = Brushes.Transparent;
+            ErrorsTextBlock.Text = "";
+        }
+    }
+}
